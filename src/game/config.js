63a55a20a4ec1317efb3f8d373da029ded779399
enamiya1126k <enamiya1126k@gmail.com>
2026-07-16 13:30:18 +0900

    Add files via upload

--- src/game/config.js
+++ src/game/config.js
@@ -1,33 +1,19 @@
-/* ABYSS DOMINION v0.0.1 Foundation */
-export const GAME_VERSION = "0.0.1";
+export const GAME_VERSION = "0.0.4";
 
-export const FLOOR_CONFIG = {
-  minFloor:1,
-  maxFloor:1000,
-  checkpointInterval:10,
-  areaInterval:50,
-  shopMinInterval:3,
-  shopMaxInterval:7,
-};
+export const MAP_CONFIG = Object.freeze({
+  columns: 31,
+  rows: 31,
+  tileSize: 48,
+});
 
-export const PARTY_CONFIG = {
-  maxPartySize:4,
-  maxStorageSize:500,
-  maxLevel:999,
-  maxPlus:99,
-  maxStars:10,
-};
-
-export const BATTLE_CONFIG = {
-  defaultSpeed:1,
-  speeds:[0.5,1,1.5,2],
-  criticalMultiplier:1.8,
-};
-
-export const CAMERA_CONFIG = {
-  minZoom:0.5,
-  maxZoom:1.5,
-  defaultZoom:1,
-};
+export const CAMERA_CONFIG = Object.freeze({
+  minZoom: 0.45,
+  maxZoom: 1.55,
+  defaultZoom: 0.9,
+  doubleTapZoom: 0.9,
+  dragThreshold: 8,
+  doubleTapDelayMs: 280,
+  edgePadding: 28,
+});
 
 export const SAVE_KEY = "abyss-dominion-save";
